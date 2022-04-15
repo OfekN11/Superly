fbb751f69d8590877a29ff85c44e0d07e6d07574
unknown <kayats@post.bgu.ac.il>
2022-04-15 09:09:42 +0300

    Adding ByOrderAgreement

--- dev/SuperLee/src/SuperLee/BusinessLayer/Agreement/ByOrderAgreement.java
+++ dev/SuperLee/src/SuperLee/BusinessLayer/Agreement/ByOrderAgreement.java
@@ -1,4 +1,33 @@
 package SuperLee.BusinessLayer.Agreement;
 
-public class ByOrderAgreement {
+import SuperLee.BusinessLayer.Item;
+
+import java.util.List;
+
+public class ByOrderAgreement extends Agreement{
+
+    private int deliveryDays;
+
+    public ByOrderAgreement(List<Item> _items, int _delivery){
+        super(_items);
+        deliveryDays = _delivery;
+    }
+
+    @Override
+    public boolean isTransporting() {
+        return true;
+    }
+
+    @Override
+    public int daysToDelivery() {
+        return deliveryDays;
+    }
+
+    public int getDeliveryDays(){
+        return deliveryDays;
+    }
+
+    public void setDeliveryDays(int _delivery){
+        deliveryDays = _delivery;
+    }
 }
