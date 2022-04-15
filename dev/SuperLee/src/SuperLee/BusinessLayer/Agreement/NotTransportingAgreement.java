4dacf469e6153881dc9113898196c2172055e639
unknown <kayats@post.bgu.ac.il>
2022-04-15 09:12:19 +0300

    Adding NotTransportingAgreement

--- dev/SuperLee/src/SuperLee/BusinessLayer/Agreement/NotTransportingAgreement.java
+++ dev/SuperLee/src/SuperLee/BusinessLayer/Agreement/NotTransportingAgreement.java
@@ -1,4 +1,22 @@
 package SuperLee.BusinessLayer.Agreement;
 
-public class NotTransportingAgreement {
+import SuperLee.BusinessLayer.Item;
+
+import java.util.List;
+
+public class NotTransportingAgreement extends Agreement {
+
+    public NotTransportingAgreement(List<Item> _items){
+        super(_items);
+    }
+
+    @Override
+    public boolean isTransporting() {
+        return false;
+    }
+
+    @Override
+    public int daysToDelivery() {
+        return -1; // means not delivering
+    }
 }
