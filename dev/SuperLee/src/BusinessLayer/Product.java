e8217361d70d8ba19781a525ecae855fac24557a
travkaie <tomerrav@post.bgu.ac.il>
2022-04-17 18:10:38 +0300

    starting to create the base for the BL

---
+++ dev/SuperLee/src/BusinessLayer/Product.java
@@ -0,0 +1,18 @@
+package BusinessLayer;
+
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.List;
+
+public class Product {
+    private int id;
+    private String name;
+    private Dictionary<Location, Integer> minAmount;
+    private Dictionary<Location, Integer> inStock;
+    private Dictionary<Location, Integer> maxAmount;
+    private int weight;
+    private List<Supplier> suppliers;
+    private double price;
+    private List<Discount> discountHistory;
+    private Date expiration;
+}
