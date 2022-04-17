5e0f16c059b692b3400c6a1e6b1bbd2aabf64ba9
maliahamir <amirmal@post.bgu.ac.il>
2022-04-17 21:50:23 +0300

    Start

--- dev/SuperLee/src/BusinessLayer/Product.java
+++ dev/SuperLee/src/BusinessLayer/Product.java
@@ -7,12 +7,24 @@ import java.util.List;
 public class Product {
     private int id;
     private String name;
-    private Dictionary<Location, Integer> minAmount;
-    private Dictionary<Location, Integer> inStock;
-    private Dictionary<Location, Integer> maxAmount;
+    private Dictionary<Location, Integer> minAmount; //min amount which indicates for lack of the specific product.
+    private Dictionary<Location, Integer> maxAmount; //max amount available to store in the warehouse for the specific product at once.
+    private Dictionary<Location, Integer> inStock; //current amount in stock (store and warehouse seperately)
     private int weight;
     private List<Supplier> suppliers;
     private double price;
     private List<Discount> discountHistory;
-    private Date expiration;
+    private List<Sale> saleHistory;
+
+    private void RemoveItems(int storeId, int amount) { //brought or thrown
+    }
+    private void MoveItem(int storeId, int amount) { //from warehouse to store
+    }
+    private void AddItems(int storeId, int amount) { //from supplier to warehouse
+    }
+    private void ReturnedToStore(int storeId, int amount) { //from customer to store
+    }
+    private void changeLocation(Location from, Location to, int amount) {
+
+    }
 }
