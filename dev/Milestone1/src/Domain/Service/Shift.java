af80a75bdc77dac244d038dbeeb7ff228ef74a4a
ikak <roi.tief97@gmail.com>
2022-04-18 16:46:01 +0300

    finished Service objects

---
+++ dev/Milestone1/src/Domain/Service/Shift.java
@@ -0,0 +1,22 @@
+package Domain.Service;
+
+import Domain.Business.Objects.Enums.ShiftType;
+
+import java.util.Date;
+
+/**
+ * Service model for Shift
+ */
+public class Shift {
+    public final Date date;
+    public final ShiftType type;
+
+    private Shift(Date date, ShiftType type){
+        this.date = date;
+        this.type = type;
+    }
+
+    public Shift(Domain.Business.Objects.Shift bShift){
+        this(bShift.getDate(), bShift.getType());
+    }
+}
