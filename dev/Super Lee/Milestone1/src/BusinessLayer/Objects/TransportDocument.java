3ed2b0fe197a0deb50d8fce167685c6e105f2035
HDChaiS <hadch@post.bgu.ac.il>
2022-04-19 12:53:37 +0300

    Add basic files: Classes: Truck, TransportDocument, Transport Enums: LicenseTypes, ShippingAreas

---
+++ dev/Super Lee/Milestone1/src/BusinessLayer/Objects/TransportDocument.java
@@ -0,0 +1,15 @@
+package BusinessLayer.Objects;
+
+import java.util.Date;
+import java.util.List;
+
+public class TransportDocument {
+    private Date date;
+    //private Time timeDeparture;
+    private int truckNumber;
+    private String driverName;
+    private String sources;
+    private String destinations;
+    private List<Integer> destsSN;
+    private int weightAtSource; // can be more than 1 source
+}
