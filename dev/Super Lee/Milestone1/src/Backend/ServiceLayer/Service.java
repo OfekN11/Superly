a720377327783f2bf693303db57e5f1e6dd6bedf
HDChaiS <hadch@post.bgu.ac.il>
2022-04-20 23:02:28 +0300

    Update service layer

---
+++ dev/Super Lee/Milestone1/src/Backend/ServiceLayer/Service.java
@@ -0,0 +1,20 @@
+package Backend.ServiceLayer;
+
+
+public class Service {
+    public Service() {
+    }
+
+    public Result addTruck(int licenseNumber, LicenseTypes model, int netWeight, int maxCapacityWeight)
+    {
+        //TODO: implement
+        return null;
+    }
+
+    public Result getTransportDocument(int transportDocumentSN)
+    {
+        //TODO: implement
+        return null;
+    }
+
+}
