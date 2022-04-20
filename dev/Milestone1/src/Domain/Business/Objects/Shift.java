5e4cc91c882af1cc1c224ba6e485b9f94e6231f8
Roi Tiefenbrunn <roitief@post.bgu.ac.il>
2022-04-20 20:26:27 +0300

    push khartabul

--- dev/Milestone1/src/Domain/Business/Objects/Shift.java
+++ dev/Milestone1/src/Domain/Business/Objects/Shift.java
@@ -14,6 +14,19 @@ public abstract class Shift {
     private Set<Employee> employees;
     private int shiftManagerId;
     private DShift dShift; // represent of this object in the DAL
+    private int carrierCount;
+    private int cashierCount;
+    private int hr_managersCount;
+    private int logistics_managersCount;
+    private int storekeeperCount;
+    private int sorterCount;
+
+    private Set<Integer> carrierIDs;
+    private Set<Integer> cashierIDs;
+    private Set<Integer> hr_managerIDs;
+    private Set<Integer> logistics_managerIDs;
+    private Set<Integer> storekeeperIDs;
+    private Set<Integer> sorterIDs;
 
     // constructors
     public Shift(Date date,Cashier shiftManager) {
@@ -69,5 +82,101 @@ public abstract class Shift {
     public void removeEmployee(Employee employee){
         if (!employees.remove(employee))
             throw new RuntimeException(String.format(employeeNotInShiftErrorMsg,employee.getId()));
+    }
+
+    public int getCarrierCount() {
+        return carrierCount;
+    }
+
+    public void setCarrierCount(int carrierCount) {
+        this.carrierCount = carrierCount;
+    }
+
+    public int getCashierCount() {
+        return cashierCount;
+    }
+
+    public void setCashierCount(int cashierCount) {
+        this.cashierCount = cashierCount;
+    }
+
+    public int getHr_managersCount() {
+        return hr_managersCount;
+    }
+
+    public void setHr_managersCount(int hr_managersCount) {
+        this.hr_managersCount = hr_managersCount;
+    }
+
+    public int getLogistics_managersCount() {
+        return logistics_managersCount;
+    }
+
+    public void setLogistics_managersCount(int logistics_managersCount) {
+        this.logistics_managersCount = logistics_managersCount;
+    }
+
+    public int getStorekeeperCount() {
+        return storekeeperCount;
+    }
+
+    public void setStorekeeperCount(int storekeeperCount) {
+        this.storekeeperCount = storekeeperCount;
+    }
+
+    public int getSorterCount() {
+        return sorterCount;
+    }
+
+    public void setSorterCount(int sorterCount) {
+        this.sorterCount = sorterCount;
+    }
+
+    public Set<Integer> getCarrierIDs() {
+        return carrierIDs;
+    }
+
+    public void setCarrierIDs(Set<Integer> carrierIDs) {
+        this.carrierIDs = new HashSet<>(carrierIDs);
+    }
+
+    public Set<Integer> getCashierIDs() {
+        return cashierIDs;
+    }
+
+    public void setCashierIDs(Set<Integer> cashierIDs) {
+        this.cashierIDs = new HashSet<>(cashierIDs);
+    }
+
+    public Set<Integer> getHr_managerIDs() {
+        return hr_managerIDs;
+    }
+
+    public void setHr_managerIDs(Set<Integer> hr_managerIDs) {
+        this.hr_managerIDs = new HashSet<>(hr_managerIDs);
+    }
+
+    public Set<Integer> getLogistics_managerIDs() {
+        return logistics_managerIDs;
+    }
+
+    public void setLogistics_managerIDs(Set<Integer> logistics_managerIDs) {
+        this.logistics_managerIDs = new HashSet<>(logistics_managerIDs);
+    }
+
+    public Set<Integer> getStorekeeperIDs() {
+        return storekeeperIDs;
+    }
+
+    public void setStorekeeperIDs(Set<Integer> storekeeperIDs) {
+        this.storekeeperIDs = new HashSet<>(storekeeperIDs);
+    }
+
+    public Set<Integer> getSorterIDs() {
+        return sorterIDs;
+    }
+
+    public void setSorterIDs(Set<Integer> sorterIDs) {
+        this.sorterIDs = new HashSet<>(sorterIDs);
     }
 }
--- dev/Milestone1/src/Domain/Service/Objects/Shift.java
+++ dev/Milestone1/src/Domain/Service/Objects/Shift.java
@@ -1,22 +1,52 @@
 package Domain.Service.Objects;
 
-import Globals.Enums.ShiftTypes;
-
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 /**
  * Service model for Shift
  */
-public class Shift {
+public abstract class Shift {
     public final Date date;
-    public final ShiftTypes type;
 
-    private Shift(Date date, ShiftTypes type){
+    public final int carrierCount;
+    public final int cashierCount;
+    public final int storekeeperCount;
+    public final int sorterCount;
+    public final int hr_managersCount;
+    public final int logistics_managersCount;
+
+    public final Set<Integer> carrierIDs;
+    public final Set<Integer> cashierIDs;
+    public final Set<Integer> storekeeperIDs;
+    public final Set<Integer> sorterIDs;
+    public final Set<Integer> hr_managerIDs;
+    public final Set<Integer> logistics_managerIDs;
+
+    private Shift(Date date,
+                  int carrierCount, int cashierCount, int storekeeperCount, int sorterCount, int hr_managersCount, int logistics_managersCount,
+                  Set<Integer> carrierIDs, Set<Integer> cashierIDs, Set<Integer> storekeeperIDs, Set<Integer> sorterIDs, Set<Integer> hr_managerIDs, Set<Integer> logistics_managerIDs){
         this.date = date;
-        this.type = type;
+
+        this.carrierCount = carrierCount;
+        this.cashierCount = cashierCount;
+        this.storekeeperCount = storekeeperCount;
+        this.sorterCount = sorterCount;
+        this.hr_managersCount = hr_managersCount;
+        this.logistics_managersCount = logistics_managersCount;
+
+        this.carrierIDs = Collections.unmodifiableSet(carrierIDs);
+        this.cashierIDs = Collections.unmodifiableSet(cashierIDs);
+        this.storekeeperIDs = Collections.unmodifiableSet(storekeeperIDs);
+        this.sorterIDs = Collections.unmodifiableSet(sorterIDs);
+        this.hr_managerIDs = Collections.unmodifiableSet(hr_managerIDs);
+        this.logistics_managerIDs = Collections.unmodifiableSet(logistics_managerIDs);
     }
 
     public Shift(Domain.Business.Objects.Shift bShift){
-        this(bShift.getDate(), bShift.getType());
+        this(bShift.getDate(),
+                bShift.getCarrierCount(), bShift.getCashierCount(), bShift.getStorekeeperCount(), bShift.getSorterCount(), bShift.getHr_managersCount(), bShift.getLogistics_managersCount(),
+                bShift.getCarrierIDs(), bShift.getCashierIDs(), bShift.getStorekeeperIDs(), bShift.getSorterIDs(), bShift.getHr_managerIDs(), bShift.getLogistics_managerIDs());
     }
 }
