8667e6f603a7f7811fc962cd250bb846181f148b
HDChaiS <hadch@post.bgu.ac.il>
2022-04-21 11:53:34 +0300

    Update

--- dev/Super Lee/Milestone1/src/Backend/ServiceLayer/Service.java
+++ dev/Super Lee/Milestone1/src/Backend/ServiceLayer/Service.java
@@ -12,8 +12,27 @@ public class Service {
         //TODO: implement
         return null;
     }
-
+    public Result updateTruck(int licenseNumber, LicenseTypes model, int netWeight, int maxCapacityWeight)
+    {
+        //TODO: implement
+        return null;
+    }
+    public Result removeTruck(int licenseNumber)
+    {
+        //TODO: implement
+        return null;
+    }
     public Result getTransportDocument(int transportDocumentSN)
+    {
+        //TODO: implement
+        return null;
+    }
+    public Result weightTruck(int transportSN)
+    {
+        //TODO: implement
+        return null;
+    }
+    public Result getDestinationDocument(int destinationDocumentSN)
     {
         //TODO: implement
         return null;
