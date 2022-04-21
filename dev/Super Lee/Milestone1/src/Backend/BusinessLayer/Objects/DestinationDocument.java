59fe0c8d2c5046b35d36e8b7061175eafc1da425
HDChaiS <hadch@post.bgu.ac.il>
2022-04-21 12:03:14 +0300

    Update

--- dev/Super Lee/Milestone1/src/Backend/BusinessLayer/Objects/DestinationDocument.java
+++ dev/Super Lee/Milestone1/src/Backend/BusinessLayer/Objects/DestinationDocument.java
@@ -3,6 +3,13 @@ package Backend.BusinessLayer.Objects;
 import java.util.List;
 
 public class DestinationDocument {
-    private static int destDocumentSN;
+    private static int incSN = 0;
+    private int destDocumentSN = 0;
     private List<String> providedProducts;
+
+    public DestinationDocument(List<String> providedProducts) {
+        destDocumentSN = incSN;
+        this.providedProducts = providedProducts;
+        incSN += 1;
+    }
 }
