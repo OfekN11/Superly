ac027daa92b9752b06b3078b7b8031d712a03c1d
travkaie <tomerrav@post.bgu.ac.il>
2022-04-23 13:53:50 +0300

    add sale for tests fixed addSale function to remove redundant copies

--- dev/SuperLee/src/BusinessLayer/InventoryController.java
+++ dev/SuperLee/src/BusinessLayer/InventoryController.java
@@ -2,6 +2,7 @@ package BusinessLayer;
 
 import BusinessLayer.DiscountsAndSales.PurchaseFromSupplier;
 import BusinessLayer.DiscountsAndSales.SaleToCustomer;
+import jdk.jfr.Percentage;
 
 import java.util.*;
 
@@ -13,12 +14,18 @@ public class InventoryController {
     private List<SaleToCustomer> sales;
     private Map<Integer, Product> products;
     private int saleID;
+    private int catID;
+    private int productID;
+    private int storeID;
     public InventoryController() {
         storeIds = new ArrayList<>();
         categories = new HashMap<>();
         sales = new ArrayList<>();
         products = new HashMap<>();
+        storeID=0;
         saleID=1;
+        catID=1;
+        productID=1;
     }
 
     public void loadTestData() {
@@ -32,8 +39,45 @@ public class InventoryController {
     }
 
     public SaleToCustomer addSale(List<Integer> categoriesList, List<Integer> productIDs, int percent, Date start, Date end) {
+        if (!start.before(end)) //could add more restrictions regarding adding past sales but would be problematic for tests
+            throw new IllegalArgumentException("Illegal dates. start must be before end");
+        if (!(percent>0 && percent<100))
+            throw new IllegalArgumentException("Percent sale must be between 1 and 99. Received: " + percent);
+        if (categoriesList.isEmpty()&&productIDs.isEmpty())
+            throw new IllegalArgumentException("Must specify categories and/or products for the sale to apply to");
+
+        //remove redundant products and categories
+        List<Integer> redundantCategories = new ArrayList<>();
+        //search forward
+        for (Integer i = 0; i<categoriesList.size(); i++) {
+            Category category1 = categories.get(i);
+            for (Integer j = i + 1; i < categoriesList.size(); j++) {
+                Category category2 = categories.get(j);
+                if (category2.belongsToCategory(category1)) {
+                    redundantCategories.add(j);
+                }
+                if (category1.belongsToCategory(category2)) {
+                    redundantCategories.add(i);
+                }
+            }
+        }
+        categoriesList.removeAll(redundantCategories);
+        //search products
+        List<Integer> redundantProducts = new ArrayList<>();
+        for (Integer i = 0; i< productIDs.size(); i++) {
+            Product product = getProduct(i);
+            for (Integer c : categoriesList) {
+                Category category = categories.get(c);
+                if (product.belongsToCategory(category)) {
+                    redundantProducts.add(i);
+                }
+            }
+        }
+        productIDs.removeAll(redundantProducts);
+
         SaleToCustomer sale = new SaleToCustomer(saleID++, start, end, percent, categoriesList, productIDs);
         sales.add(sale);
+
         Product product = null;
         for (Integer pID: productIDs) {
             //should we throw error if only one of them is illegal
@@ -170,15 +214,9 @@ public class InventoryController {
     }
 
     public int addStore() {
-        if (storeIds.isEmpty()) {
-            storeIds.add(0);
-            return 0;
-        }
-        else {
-            int id = max(storeIds) + 1;
-            storeIds.add(id);
-            return id;
-        }
+        int id = storeID++;
+        storeIds.add(id);
+        return id;
     }
 
     public void removeStore(int storeID) {
@@ -208,7 +246,7 @@ public class InventoryController {
 
     public Product newProduct(String name, int categoryID, int weight, double price, Map<Integer, Integer> suppliers, int manufacturerID) {
         Category category = categories.get(categoryID);
-        int id = products.size() + 1;
+        int id = productID++;
         Product product = new Product(id, name, category, weight, price, suppliers, manufacturerID);
         products.put(id, product);
         return product;
@@ -293,15 +331,15 @@ public class InventoryController {
 
     private void addCategoriesForTests () {
         Category cSmall1 = new Category("Small", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cSmall1);
+        categories.put(catID++, cSmall1);
         Category cSmall2 = new Category("Small", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cSmall2);
+        categories.put(catID++, cSmall2);
         Category cLarge1 = new Category("Large", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cLarge1);
+        categories.put(catID++, cLarge1);
         Category cLarge2 = new Category("Large", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cLarge2);
+        categories.put(catID++, cLarge2);
         Category cMedium = new Category("Medium", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cMedium);
+        categories.put(catID++, cMedium);
         Category cShampoo = new Category("Shampoo", new HashSet<>(), new ArrayList<>(), null);
         Set<Category> milk = new HashSet<>();
         milk.add(cSmall1);
@@ -316,46 +354,46 @@ public class InventoryController {
         dairy.add(cMilk);
         dairy.add(cYogurt);
         Category cdairy = new Category("Dairy", dairy, new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cdairy);
-        categories.put(categories.size() + 1, cShampoo);
+        categories.put(catID++, cdairy);
+        categories.put(catID++, cShampoo);
         Category cToothpaste = new Category("Toothpaste", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cToothpaste);
+        categories.put(catID++, cToothpaste);
         Set<Category> health = new HashSet<>();
         health.add(cShampoo);
         health.add(cToothpaste);
         Category cHealth = new Category("Health,", health, new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cHealth);
+        categories.put(catID++, cHealth);
         Category cOrg1 = new Category("Organic", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cOrg1);
+        categories.put(catID++, cOrg1);
         Category cOrg2 = new Category("Organic", new HashSet<>(), new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cOrg2);
+        categories.put(catID++, cOrg2);
         Set<Category> veggies = new HashSet();
         veggies.add(cOrg1);
         Set<Category> fruits = new HashSet();
         fruits.add(cOrg2);
         Category cVeggie = new Category("Vegetables", veggies, new ArrayList<>(), null);
         //12
-        categories.put(categories.size() + 1, cVeggie);
+        categories.put(catID++, cVeggie);
         Category cFruit = new Category("Fruits", veggies, new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cFruit);
+        categories.put(catID++, cFruit);
         Set<Category> produce = new HashSet<>();
         produce.add(cFruit);
         produce.add(cVeggie);
         Category cProduce = new Category("Produce", produce, new ArrayList<>(), null);
-        categories.put(categories.size() + 1, cProduce);
+        categories.put(catID++, cProduce);
     }
 
     private void addProductsForTests () {
-        products.put(products.size()+1, new Product(products.size()+1, "tomato", categories.get(12), -1, 7.2, new HashMap<>(), 0));
-        products.put(products.size()+1, new Product(products.size()+1, "tomato", categories.get(10), -1, 9.2, new HashMap<>(), 0));
-        products.put(products.size()+1, new Product(products.size()+1, "strawberry", categories.get(11), -1, 7.2, new HashMap<>(), 0));
-        products.put(products.size()+1, new Product(products.size()+1, "melon", categories.get(13), -1, 7.2, new HashMap<>(), 0));
-        products.put(products.size()+1, new Product(products.size()+1, "Hawaii", categories.get(6), 1.2, 13, new HashMap<>(), 1));
-        products.put(products.size()+1, new Product(products.size()+1, "Crest", categories.get(9), 0.7, 7.2, new HashMap<>(), 2));
-        products.put(products.size()+1, new Product(products.size()+1, "Tara 1L", categories.get(5), 1.2, 8.6, new HashMap<>(), 17));
-        products.put(products.size()+1, new Product(products.size()+1, "Tnuva 1L", categories.get(5), 1.2, 8, new HashMap<>(), 18));
-        products.put(products.size()+1, new Product(products.size()+1, "yoplait strawberry", categories.get(2), 0.5, 5.3, new HashMap<>(), 9));
-        products.put(products.size()+1, new Product(products.size()+1, "yoplait vanilla", categories.get(2), 0.5, 5.3, new HashMap<>(), 9));
+        products.put(productID, new Product(productID++, "tomato", categories.get(12), -1, 7.2, new HashMap<>(), 0));
+        products.put(productID, new Product(productID++, "tomato", categories.get(10), -1, 9.2, new HashMap<>(), 0));
+        products.put(productID, new Product(productID++, "strawberry", categories.get(11), -1, 7.2, new HashMap<>(), 0));
+        products.put(productID, new Product(productID++, "melon", categories.get(13), -1, 7.2, new HashMap<>(), 0));
+        products.put(productID, new Product(productID++, "Hawaii", categories.get(7), 1.2, 13, new HashMap<>(), 1));
+        products.put(productID, new Product(productID++, "Crest", categories.get(8), 0.7, 7.2, new HashMap<>(), 2));
+        products.put(productID, new Product(productID++, "Tara 1L", categories.get(5), 1.2, 8.6, new HashMap<>(), 17));
+        products.put(productID, new Product(productID++, "Tnuva 1L", categories.get(5), 1.2, 8, new HashMap<>(), 18));
+        products.put(productID, new Product(productID++, "yoplait strawberry", categories.get(2), 0.5, 5.3, new HashMap<>(), 9));
+        products.put(productID, new Product(productID++, "yoplait vanilla", categories.get(2), 0.5, 5.3, new HashMap<>(), 9));
         for (int i : storeIds) {
             List<Integer> shelves = new ArrayList<>();
             shelves.add(2*i); shelves.add(2*i+1);
@@ -367,7 +405,25 @@ public class InventoryController {
     }
 
     private void addSalesForTests () {
-
+        List<Integer> categories = new ArrayList<>();
+        List<Integer> products = new ArrayList<>();
+        List<Integer> empty = new ArrayList<>();
+        //small milk,       medium milk,         toothpaste,         health
+        categories.add(1); categories.add(5); categories.add(8); categories.add(9);
+        //crest             tara1L          tnuva1L         organic tomato  organic strawberry
+        products.add(6); products.add(7); products.add(8); products.add(2); products.add(3);
+        Date threeDaysAgo = new Date(); threeDaysAgo.setHours(-72);
+        Date twoDaysAgo = new Date(); twoDaysAgo.setHours(-48);
+        Date yesterday = new Date(); yesterday.setHours(-24);
+        Date today = new Date();
+        Date tomorrow = new Date(); tomorrow.setHours(24);
+        Date twoDays = new Date(); twoDays.setHours(48);
+        Date threeDays = new Date(); threeDays.setHours(72);
+        addSale(categories, products, 15, threeDaysAgo, tomorrow);
+        addSale(categories, empty, 20, threeDaysAgo, yesterday);
+        addSale(empty, products, 5, today, tomorrow);
+        addSale(categories, products, 12, tomorrow, threeDays);
+        addSale(categories, empty, 17, twoDaysAgo, twoDays);
     }
 
     private void addReportsForTests () {
@@ -405,7 +461,7 @@ public class InventoryController {
     }
 
     public Category addCategory(String name, int parentCategoryID) {
-        int id = categories.size()+1;
+        int id = catID++;
         if (parentCategoryID==0) {
             categories.put(id, new Category(name, new HashSet<>(), new ArrayList<>(), null));
         }
