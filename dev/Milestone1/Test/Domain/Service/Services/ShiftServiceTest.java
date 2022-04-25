9b5044f2b8c735374ab07a39c86ba9174db01b58
Novo <novo@post.bgu.ac.il>
2022-04-25 12:35:43 +0300

    few tests

---
+++ dev/Milestone1/Test/Domain/Service/Services/ShiftServiceTest.java
@@ -0,0 +1,32 @@
+package Domain.Service.Services;
+
+import Domain.Business.Controllers.ShiftController;
+import Domain.Service.Objects.Shift;
+import Globals.Enums.ShiftTypes;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.*;
+
+public class ShiftServiceTest {
+
+    @Test
+    public void shiftManagerTest() {
+        ShiftService shiftService = new ShiftService();
+        shiftService.loadData();
+        try {
+            assertTrue(shiftService.createShift(new SimpleDateFormat("dd-MM-yyyy").parse("25-07-1998"), ShiftTypes.Morning,"5",6,6,6,4,4,4).isOk());
+            Shift shift = shiftService.getShift(new SimpleDateFormat("dd-MM-yyyy").parse("25-07-1998"), ShiftTypes.Morning).getValue();
+            assertTrue(shift.cashierIDs.size()>0);
+            assertNotNull(shift.shiftManagerId);
+            assertNotEquals("",shift.shiftManagerId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void createIllegalShiftTest() {
+    }
+}
