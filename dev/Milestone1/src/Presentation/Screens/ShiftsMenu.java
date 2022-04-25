ab1211862aa763763170b0225218f4ad11b76a6d
Novo <novo@post.bgu.ac.il>
2022-04-26 00:24:50 +0300

    fix add shift Bug

--- dev/Milestone1/src/Presentation/Screens/ShiftsMenu.java
+++ dev/Milestone1/src/Presentation/Screens/ShiftsMenu.java
@@ -379,7 +379,7 @@ public class ShiftsMenu extends Screen {
         while (type == null) {
             System.out.println("\nEnter shift's type");
             for (int i = 0; i < ShiftTypes.values().length; i++)
-                System.out.println((i + 1) + " -- " + JobTitles.values()[i]);
+                System.out.println((i + 1) + " -- " + ShiftTypes.values()[i]);
             try {
                 int ordinal = Integer.parseInt(scanner.nextLine());
                 if (ordinal == -1) {
