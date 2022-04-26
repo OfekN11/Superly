87db94ee7ce592a56ba9fb334843da29d2a34f1d
HDChaiS <hadch@post.bgu.ac.il>
2022-04-26 13:31:41 +0300

    Update frontend side

--- dev/Super Lee/Milestone1/src/Frontend/Cli.java
+++ dev/Super Lee/Milestone1/src/Frontend/Cli.java
@@ -11,37 +11,191 @@ public class Cli {
     private static Service service = new Service();
     public static void main(String[] args)
     {
-        //login();
-        switch (mainMenu())
-        {
-            case 1:
-                transportManager();
-                break;
-            case 2:
-                //TODO: Truck system management
-                break;
-            default:
+        boolean closeProgram = false;
+        while(!closeProgram) {
+            switch (mainMenu()) {
+                case 1:
+                    driverSystemManagement();
+                    break;
+                case 2:
+                    truckSystemManagement();
+                    break;
+                case 3:
+                    transportSystemManagement();
+                    break;
+                case 4:
+                    documentSystemManagement();
+                    break;
+                case 5:
+                    closeProgram = true;
+                    System.out.println("Good Bye!!!");
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
         }
     }
-    private void login()
+    private static void transportSystemManagement()
     {
-        System.out.println("Login:");
-        System.out.println("Please enter your details:");
-        System.out.print("Username: ");
-        String username = reader.next();
-        System.out.println("Password: ");
-        String password = reader.next();
+        boolean back = false;
+        while(!back) {
+            switch (menuTransSM()) {
+                case 1:
+                    //TODO: service.addTransportOrder();
+                    break;
+                case 2:
+                    createNewTransport();
+                    break;
+                case 3:
+                    //TODO: service.getWaitingTransports();
+                    break;
+                case 4:
+                    //TODO: service.getInProgressTransports();
+                    break;
+                case 5:
+                    //TODO: service.getRedesignTransports();
+                    break;
+                case 6:
+                    updateTransport();
+                    break;
+                case 7:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
+        }
+    }
 
+    private static void updateTransport() {
+        boolean back = false;
+        while(!back) {
+            switch (menuCNT()) {
+                case 1:
+                    //TODO: service.placeDriver();
+                    break;
+                case 2:
+                    //TODO: service.placeTruck();
+                    break;
+                case 3:
+                    //TODO: service.startTransport();
+                    break;
+                case 4:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
+        }
+    }
+
+    private static void createNewTransport() {
+        boolean back = false;
+        while(!back) {
+            switch (menuCNT()) {
+                case 1:
+                    //TODO: service.getOrders();
+                    break;
+                case 2:
+                    //TODO: service.getOrdersInTransportArea();
+                    break;
+                case 3:
+                    //TODO: service.addOrderToTransport();
+                    break;
+                case 4:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
+        }
+    }
+
+    private static int menuCNT() {
+        System.out.println("Create Transport\n" +
+                "Please enter option number:\n" +
+                "1. View orders\n" +
+                "2. View orders in same areas\n" +
+                "3. Add order\n" +
+                "4. Back");
+        return getChoice(1, 4);
+    }
+
+    private static int menuTransSM() {
+        System.out.println("Transport system management\n" +
+                "Please enter option number:\n" +
+                "1. Add transport order\n" +
+                "2. Create new transport\n" +
+                "3. Get waiting transport\n" +
+                "4. Get in progress transport\n" +
+                "5. Get redesign transport\n" +
+                "6. Update transport\n" +
+                "7. Back");
+        return getChoice(1, 7);
+    }
+
+    private static void documentSystemManagement() {
+        boolean back = false;
+        while(!back) {
+            switch (menuDocSM()) {
+                case 1:
+                    //TODO: service.getTransportDocument(int docSN);
+                    break;
+                case 2:
+                    //TODO: service.getDestinationDocument(int docSN);
+                    break;
+                case 3:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
+        }
+    }
+
+    private static int menuDocSM() {
+        System.out.println("Document system management\n" +
+                "Please enter option number:\n" +
+                "1. Get transport document\n" +
+                "2. Get destination document\n" +
+                "3. Back");
+        return getChoice(1, 3);
+    }
+
+    private static void driverSystemManagement() {
+        boolean back = false;
+        while(!back) {
+            switch (menuDSM()) {
+                case 1:
+                    //TODO: service.addDriver();
+                    break;
+                case 2:
+                    //TODO: service.removeDriver();
+                    break;
+                case 3:
+                    //TODO: service.updateDriver();
+                    break;
+                case 4:
+                    back = true;
+                    break;
+                default:
+                    //TODO:
+                    System.out.println("Invalid input!");
+            }
+        }
     }
 
     private static int mainMenu()
     {
-        System.out.println("Main menu:\n1. Transport Manager\n2. Truck Driver");
-        return getChoice(1, 2);
+        System.out.println("Welcome to Transport's system\n" +
+                "Please enter option number:\n" +
+                "1. Driver system management\n" +
+                "2. Truck system management\n" +
+                "3. Transport system management\n" +
+                "4. Document system management\n" +
+                "5. Exit");
+        return getChoice(1, 5);
     }
-
-
-
 
     private static int getChoice(int a, int b)
     {
@@ -53,38 +207,85 @@ public class Cli {
         return choice;
     }
 
-    private static int mainMenuTM()
+    private static int menuDSM()
     {
-        System.out.println("Transport Manager menu:\n1. Transport Management\n" +
-                "2. Truck system management\n3. Document system management\n4. Back...");
+        System.out.println("Driver system management\n" +
+                "Please enter option number:\n" +
+                "1. Add driver\n" +
+                "2. Delete driver\n" +
+                "3. Update driver lisence type\n" +
+                "4. Back");
+        return getChoice(1, 4);
+    }
+
+    private static int menuTSM()
+    {
+        System.out.println("Truck system management:\n" +
+                "1. Add truck\n" +
+                "2. Delete truck\n" +
+                "3. Back");
         return getChoice(1, 3);
     }
-
-    private static void transportManager()
+    private static void truckSystemManagement()
     {
-        switch (mainMenuTM())
-        {
-            case 1:
-                transportManagement();
-                break;
-            case 2:
-                truckSystemManagement();
-                break;
-            case 3:
-                //documentSystemManagement();
-                break;
-            default:
+        boolean back = false;
+        while(!back) {
+            switch (menuTSM())
+            {
+                case 1:
+                    addTruck();
+                    break;
+                case 2:
+                    removeTruck();
+                    break;
+                case 3:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Invalid input!");
+            }
         }
     }
-
-    private static int menuTM()
+    private static void addTruck()
     {
-        System.out.println("Transport Manager menu:\n1. Transport Order\n2. Transport system management");
-        return getChoice(1, 2);
+        //TODO: Check validation of the input
+        System.out.println("Enter truck license number:");
+        int licenseNumber = reader.nextInt();
+        LicenseTypes truckModel = getTruckModel();
+        System.out.println("Enter the weight of the truck:");
+        int netWeight = reader.nextInt();
+        System.out.println("Enter the maximum weight that a truck can load:");
+        int maxCapacityWeight = reader.nextInt();
+        service.addTruck(licenseNumber, truckModel, netWeight, maxCapacityWeight);
+        //TODO: Take the result
     }
+    private static void removeTruck()
+    {
+        //TODO: Check validation of the input
+        System.out.println("Enter truck license number:");
+        int licenseNumber = reader.nextInt();
+        service.removeTruck(licenseNumber);
+        //TODO: Take the result
+    }
+    private static LicenseTypes getTruckModel()
+    {
+        String[] truckModel = {"B", "C1", "C", "CE"};
+        System.out.println("Enter truck model:\n" +
+                "1. B\n" +
+                "2. C1\n" +
+                "3. C\n" +
+                "4. C+E");
+
+        return LicenseTypes.valueOf(truckModel[getChoice(1, 4) - 1]);
+    }
+
+
+    //-----------------------------------------------------
+    /*
     private static void transportManagement()
     {
-        switch (menuTM())
+
+        switch (1)
         {
             case 1:
                 TransportOrder to = getTransportOrder();
@@ -96,10 +297,7 @@ public class Cli {
             default:
         }
     }
-    private static void transportSystemManagement()
-    {
-        //TODO
-    }
+
     private static TransportOrder getTransportOrder()
     {
         //TODO: Check validation of the input and enable to get input of many dests and srcs
@@ -158,58 +356,9 @@ public class Cli {
         }while(!close);
     }
 
-    private static int menuTSM()
-    {
-        System.out.println("Truck System Manager menu:\n" +
-                "1. Add new truck\n" +
-                "2. Remove truck");
-        return getChoice(1, 2);
-    }
-    private static void truckSystemManagement()
-    {
-        switch (menuTSM())
-        {
-            case 1:
-                addTruck();
-                break;
-            case 2:
-                removeTruck();
-                break;
-            default:
-        }
-    }
-    private static void addTruck()
-    {
-        //TODO: Check validation of the input
-        System.out.println("Enter truck license number:");
-        int licenseNumber = reader.nextInt();
-        LicenseTypes truckModel = getTruckModel();
-        System.out.println("Enter the weight of the truck:");
-        int netWeight = reader.nextInt();
-        System.out.println("Enter the maximum weight that a truck can load:");
-        int maxCapacityWeight = reader.nextInt();
-        service.addTruck(licenseNumber, truckModel, netWeight, maxCapacityWeight);
-        //TODO: Take the result
-    }
-    private static void removeTruck()
-    {
-        //TODO: Check validation of the input
-        System.out.println("Enter truck license number:");
-        int licenseNumber = reader.nextInt();
-        service.removeTruck(licenseNumber);
-        //TODO: Take the result
-    }
-    private static LicenseTypes getTruckModel()
-    {
-        String[] truckModel = {"B", "C1", "C", "CE"};
-        System.out.println("Enter truck model:\n" +
-                "1. B\n" +
-                "2. C1\n" +
-                "3. C\n" +
-                "4. C+E");
 
-        return LicenseTypes.valueOf(truckModel[getChoice(1, 4) - 1]);
-    }
+
+
     private int mainMenuTD()
     {
         System.out.println("Truck driver menu:\n" +
@@ -231,6 +380,7 @@ public class Cli {
     }
     private void weightReport()
     {
-        
+
     }
+    */
 }
