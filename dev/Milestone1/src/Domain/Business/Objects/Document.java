874b39b72491d771463f4d4aa6a453f88bbc23db
eldad <vaskere@post.bgu.ac.il>
2022-05-08 20:52:40 +0300

    package arrangement

--- dev/Milestone1/src/Domain/Business/Objects/Document.java
+++
@@ -1,7 +0,0 @@
-package Backend.BusinessLayer.Objects;
-
-import Backend.ServiceLayer.Factory.ServiceDocumentFactory;
-
-public abstract class Document {
-    public abstract Backend.ServiceLayer.Objects.Document accept(ServiceDocumentFactory factory);
-}
