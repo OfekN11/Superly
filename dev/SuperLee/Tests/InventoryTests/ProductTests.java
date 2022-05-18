d83992f8154441f98659435137db6c08ac3d6b27
travkaie <tomerrav@post.bgu.ac.il>
2022-05-18 13:32:39 +0300

    all tests pass and clean after themselves

--- dev/SuperLee/Tests/InventoryTests/ProductTests.java
+++ dev/SuperLee/Tests/InventoryTests/ProductTests.java
@@ -3,20 +3,39 @@ package InventoryTests;
 import Domain.BusinessLayer.Inventory.Category;
 import Domain.BusinessLayer.Inventory.SaleToCustomer;
 import Domain.BusinessLayer.Inventory.Product;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import Domain.BusinessLayer.InventoryController;
+import Domain.PersistenceLayer.Controllers.CategoryDataMapper;
+import Domain.PersistenceLayer.Controllers.ProductDataMapper;
+import Domain.PersistenceLayer.Controllers.StockReportDataMapper;
+import Domain.PersistenceLayer.Controllers.StoreDAO;
+import PresentationLayer.Screens.InventoryScreens.StockReports;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
 import java.util.*;
 
-public class ProductTests {
-    Category category0 = new Category(0,"Milk", new HashSet<>(), new ArrayList<>(), null);
-    Product product0 = new Product(0, "Milk-Tnuva-1L", category0, 1, 4.5, "18");
-    Product product1 = new Product(1, "Milk-Tara-1L", category0, 1.0, 4, "25");
+import static java.util.Collections.max;
 
+public class ProductTests {
+    static Category category0;
+    static Product product0;
+    static Product product1;
+    private static InventoryController is;
+    private static int maxStoreCount;
+
+    @BeforeAll
+    public static void getMaxStoreCount() {
+        is = new InventoryController();
+        maxStoreCount = max(is.getStoreIDs());
+        is.addStore();
+        is.addStore();
+    }
     @BeforeEach
     void addLocations() {
+        is = new InventoryController();
+        category0 = is.addCategory("Test-Milk",  0);
+        product0 = is.newProduct("Test-Milk-Tnuva-1L", category0.getID(), 1, 4.5, "18");
+        product1 = is.newProduct("Test-Milk-Tara-1L", category0.getID(), 1.0, 4, "25");
         List<Integer> shelves1 = new LinkedList<Integer>();
         List<Integer> shelves2 = new LinkedList<Integer>();
         List<Integer> shelves3 = new LinkedList<Integer>();
@@ -33,110 +52,137 @@ public class ProductTests {
         shelves5.add(10);
         shelves6.add(40);
         shelves6.add(39);
-        product0.addLocation(1, shelves1, shelves2, 80, 300);
-        product0.addLocation(2, shelves3, shelves4, 65, 250);
-        product1.addLocation(1, shelves5, shelves6, 40, 150);
+        product0.addLocation(maxStoreCount+1, shelves1, shelves2, 80, 300);
+        product0.addLocation(maxStoreCount+2, shelves3, shelves4, 65, 250);
+        product1.addLocation(maxStoreCount+1, shelves5, shelves6, 40, 150);
+    }
+
+    @AfterAll
+    public static void removeData() {
+        ProductDataMapper pdm = new ProductDataMapper();
+        pdm.removeTestProducts();
+        CategoryDataMapper cdm = new CategoryDataMapper();
+        cdm.removeTestCategories();
+        StoreDAO storeDAO = new StoreDAO();
+        Collection<Integer> stores = storeDAO.getAll();
+        for (int store : stores) {
+            if (store > maxStoreCount) {
+                try {
+                    storeDAO.remove(store);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @AfterEach
+    public void removeLocationData() {
+        List<Integer> products = Arrays.asList(product0.getId(), product1.getId());
+        StoreDAO storeDAO = new StoreDAO();
+        Collection<Integer> stores = storeDAO.getAll();
+        StockReportDataMapper srdm = new StockReportDataMapper();
+        for (int store : stores) {
+            for (Integer p : products) {
+                srdm.remove(store, p);
+            }
+        }
     }
 
     @Test
     public void testAddItems() {
-        Assertions.assertEquals(0, product0.getInStore(1));
-        Assertions.assertEquals(0, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        //product0.addItems(1, new Date(), 1, 100, 5, 5, 3);
-        Assertions.assertEquals(0, product0.getInStore(1));
-        Assertions.assertEquals(100, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        //product0.addItems(1, new Date(), 1, 40, 5, 5, 4);
-        //product0.addItems(2, new Date(), 1, 30, 5, 5, 5);
-        //product1.addItems(1, new Date(), 1, 99, 5, 5, 6);
-        Assertions.assertEquals(0, product0.getInStore(1));
-        Assertions.assertEquals(140, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(30, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(99, product1.getInWarehouse(1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        product0.addItems(maxStoreCount+1, 100);
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(100, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        product0.addItems(maxStoreCount+1, 40);
+        product0.addItems(maxStoreCount+2, 30);
+        product1.addItems(maxStoreCount+1, 99);
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(140, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(30, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(99, product1.getInWarehouse(maxStoreCount+1));
     }
     @Test
     public void testMoveItems() {
-        Assertions.assertEquals(0, product0.getInStore(1));
-        Assertions.assertEquals(0, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        //product0.addItems(1, new Date(), 1, 100, 5, 5, 7);
-        product0.moveItems(1, 58);
-        Assertions.assertEquals(58, product0.getInStore(1));
-        Assertions.assertEquals(42, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        //product0.addItems(1, new Date(), 1, 40, 5, 5, 6);
-        //product0.addItems(2, new Date(), 1, 30, 5, 5, 8);
-        //product1.addItems(1, new Date(), 1, 99, 5, 5, 9);
-        product0.moveItems(1, 40);
-        product0.moveItems(2, 25);
-        product1.moveItems(1, 18);
-        Assertions.assertEquals(98, product0.getInStore(1));
-        Assertions.assertEquals(42, product0.getInWarehouse(1));
-        Assertions.assertEquals(25, product0.getInStore(2));
-        Assertions.assertEquals(5, product0.getInWarehouse(2));
-        Assertions.assertEquals(18, product1.getInStore(1));
-        Assertions.assertEquals(81, product1.getInWarehouse(1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        product0.addItems(maxStoreCount+1, 100);
+        product0.moveItems(maxStoreCount+1, 58);
+        Assertions.assertEquals(58, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(42, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        product0.addItems(maxStoreCount+1, 40);
+        product0.addItems(maxStoreCount+2, 30);
+        product1.addItems(maxStoreCount+1, 99);
+        product0.moveItems(maxStoreCount+1, 40);
+        product0.moveItems(maxStoreCount+2, 25);
+        product1.moveItems(maxStoreCount+1, 18);
+        Assertions.assertEquals(98, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(42, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(25, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(5, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(18, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(81, product1.getInWarehouse(maxStoreCount+1));
     }
     @Test
     public void testRemoveItems() {
-        Assertions.assertEquals(0, product0.getInStore(1));
-        Assertions.assertEquals(0, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        //product0.addItems(1, new Date(), 1, 100, 5, 5, 8);
-        product0.moveItems(1, 58);
-        product0.removeItems(1, 37, false);
-        Assertions.assertEquals(21, product0.getInStore(1));
-        Assertions.assertEquals(42, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        //product0.addItems(1, new Date(), 1, 40, 5, 5, 3);
-        //product0.addItems(2, new Date(), 1, 30, 5, 5, 5);
-        //product1.addItems(1, new Date(), 1, 99, 5, 5, 1);
-        product0.moveItems(1, 40);
-        product0.moveItems(2, 25);
-        product1.moveItems(1, 18);
-        product0.removeItems(1, 15, false);
-        product0.removeItems(2, 25, false);
-        product1.removeItems(1, 0, false);
-        Assertions.assertEquals(46, product0.getInStore(1));
-        Assertions.assertEquals(42, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(5, product0.getInWarehouse(2));
-        Assertions.assertEquals(18, product1.getInStore(1));
-        Assertions.assertEquals(81, product1.getInWarehouse(1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        product0.addItems(maxStoreCount+1, 100);
+        product0.moveItems(maxStoreCount+1, 58);
+        product0.removeItems(maxStoreCount+1, 37, false);
+        Assertions.assertEquals(21, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(42, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        product0.addItems(maxStoreCount+1, 40);
+        product0.addItems(maxStoreCount+2, 30);
+        product1.addItems(maxStoreCount+1, 99);
+        product0.moveItems(maxStoreCount+1, 40);
+        product0.moveItems(maxStoreCount+2, 25);
+        product1.moveItems(maxStoreCount+1, 18);
+        product0.removeItems(maxStoreCount+1, 15, false);
+        product0.removeItems(maxStoreCount+2, 25, false);
+        product1.removeItems(maxStoreCount+1, 0, false);
+        Assertions.assertEquals(46, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(42, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(5, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(18, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(81, product1.getInWarehouse(maxStoreCount+1));
     }
     @Test
     public void testReturnItems() {
         LocalDate yesterday = LocalDate.now().minusDays(1);
-
         LocalDate beforeTwoDays = LocalDate.now().minusDays(2);
-
         LocalDate beforeThreeDays = LocalDate.now().minusDays(3);
-
         LocalDate beforeFourDays = LocalDate.now().minusDays(4);
-
         LocalDate beforeFiveDays = LocalDate.now().minusDays(5);
-
         LocalDate beforeSixDays = LocalDate.now().minusDays(6);
 
         SaleToCustomer sale0 = new SaleToCustomer(0, beforeFourDays, beforeTwoDays, 30, new LinkedList<>(), new ArrayList<>());
@@ -150,35 +196,35 @@ public class ProductTests {
         category0.addSale(sale2);
         category0.addSale(sale3);
 
-        Assertions.assertEquals(0, product0.getInStore(1));
-        Assertions.assertEquals(0, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
-        double returnedValue = product0.returnItems(1, 100, beforeThreeDays);
-        Assertions.assertEquals(100, product0.getInStore(1));
-        Assertions.assertEquals(0, product0.getInWarehouse(1));
-        Assertions.assertEquals(0, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(0, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
+        double returnedValue = product0.returnItems(maxStoreCount+1, 100, beforeThreeDays);
+        Assertions.assertEquals(100, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(0, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
         Assertions.assertEquals(270 ,returnedValue);
-        returnedValue = product0.returnItems(1, 40, beforeFourDays);
+        returnedValue = product0.returnItems(maxStoreCount+1, 40, beforeFourDays);
         Assertions.assertEquals(90 ,returnedValue);
-        returnedValue = product0.returnItems(2, 30, beforeTwoDays);
+        returnedValue = product0.returnItems(maxStoreCount+2, 30, beforeTwoDays);
         Assertions.assertEquals(27 ,returnedValue);
-        returnedValue = product1.returnItems(1, 99, beforeSixDays);
+        returnedValue = product1.returnItems(maxStoreCount+1, 99, beforeSixDays);
         Assertions.assertEquals(396 ,returnedValue);
-        returnedValue = product1.returnItems(1, 5, beforeTwoDays);
+        returnedValue = product1.returnItems(maxStoreCount+1, 5, beforeTwoDays);
         Assertions.assertEquals(4 ,returnedValue);
-        returnedValue = product0.returnItems(1, 99, beforeSixDays);
+        returnedValue = product0.returnItems(maxStoreCount+1, 99, beforeSixDays);
         Assertions.assertEquals(445.5 ,returnedValue);
-        Assertions.assertEquals(239, product0.getInStore(1));
-        Assertions.assertEquals(0, product0.getInWarehouse(1));
-        Assertions.assertEquals(30, product0.getInStore(2));
-        Assertions.assertEquals(0, product0.getInWarehouse(2));
-        Assertions.assertEquals(104, product1.getInStore(1));
-        Assertions.assertEquals(0, product1.getInWarehouse(1));
+        Assertions.assertEquals(239, product0.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+1));
+        Assertions.assertEquals(30, product0.getInStore(maxStoreCount+2));
+        Assertions.assertEquals(0, product0.getInWarehouse(maxStoreCount+2));
+        Assertions.assertEquals(104, product1.getInStore(maxStoreCount+1));
+        Assertions.assertEquals(0, product1.getInWarehouse(maxStoreCount+1));
     }
 }
