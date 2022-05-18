19a9f3b5296b92ce58f9167aeea8f1b5d71207da
Novo <novo@post.bgu.ac.il>
2022-05-18 21:16:01 +0300

    adding PresentationShiftBuilder

--- dev/Milestone1/src/Presentation/Screens/ShiftsMenu.java
+++ dev/Milestone1/src/Presentation/Screens/ShiftsMenu.java
@@ -5,6 +5,7 @@ import Domain.Service.Objects.Shift;
 import Globals.Enums.Certifications;
 import Globals.Enums.ShiftTypes;
 import Globals.util.ShiftComparator;
+import Presentation.PresentationShiftBuilder;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -81,231 +82,22 @@ public class ShiftsMenu extends Screen {
     }
 
     private void addShifts() throws Exception {
-        while (true) {
-            System.out.println("Enter details to create shift (enter -1 at any point to stop the process)");
-
-            //Date
-            LocalDate date = null;
-            boolean success = false;
-            while (!success) {
-                System.out.println("\nEnter shift's date");
-                date = buildDate();
-                if (date == null)
-                    return;
-                System.out.println("Entered date: " + new SimpleDateFormat("dd-MM-yyyy").format(date));
-                success = areYouSure();
-            }
-            System.out.println("Chosen starting date: " + new SimpleDateFormat("dd-MM-yyyy").format(date));
-
-            //ShiftType
-            ShiftTypes type = null;
-            success = false;
-            while (!success) {
-                System.out.println("\nEnter shift's type");
-                for (int i = 0; i < ShiftTypes.values().length; i++)
-                    System.out.println((i + 1) + " -- " + ShiftTypes.values()[i]);
-                try {
-                    int ordinal = Integer.parseInt(scanner.nextLine());
-                    if (ordinal == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else if (ordinal < 1 || ordinal > ShiftTypes.values().length)
-                        System.out.println("Please enter an integer between 1 and " + ShiftTypes.values().length);
-                    else {
-                        type = ShiftTypes.values()[ordinal - 1];
-                        System.out.println("Entered type: " + type);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter an integer between 1 and " + ShiftTypes.values().length);
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen shift type: " + type);
-
-            //Carrier Count
-            int carrierCount = 0;
-            success = false;
-            while (!success) {
-                System.out.println("\nHow many carriers do you need for this shift?");
-                try {
-                    carrierCount = Integer.parseInt(scanner.nextLine());
-                    if (carrierCount == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else {
-                        System.out.println("Entered count: " + carrierCount);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter a non-negative integer");
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen carrier count: " + carrierCount);
-
-            //Cashier Count
-            int cashierCount = 0;
-            success = false;
-            while (!success) {
-                System.out.println("\nHow many cashiers do you need for this shift?");
-                try {
-                    cashierCount = Integer.parseInt(scanner.nextLine());
-                    if (cashierCount == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else {
-                        System.out.println("Entered count: " + cashierCount);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter a non-negative integer");
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen cashier count: " + cashierCount);
-
-            //Storekeeper Count
-            int storekeeperCount = 0;
-            success = false;
-            while (!success) {
-                System.out.println("\nHow many storekeepers do you need for this shift?");
-                try {
-                    storekeeperCount = Integer.parseInt(scanner.nextLine());
-                    if (storekeeperCount == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else {
-                        System.out.println("Entered count: " + storekeeperCount);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter a non-negative integer");
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen storekeeper count: " + storekeeperCount);
-
-            //Sorter Count
-            int sorterCount = 0;
-            success = false;
-            while (!success) {
-                System.out.println("\nHow many sorters do you need for this shift?");
-                try {
-                    sorterCount = Integer.parseInt(scanner.nextLine());
-                    if (sorterCount == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else {
-                        System.out.println("Entered count: " + sorterCount);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter a non-negative integer");
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen sorter count: " + sorterCount);
-
-            //HR Manager Count
-            int hr_managerCount = 0;
-            success = false;
-            while (!success) {
-                System.out.println("\nHow many HR managers do you need for this shift?");
-                try {
-                    hr_managerCount = Integer.parseInt(scanner.nextLine());
-                    if (hr_managerCount == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else {
-                        System.out.println("Entered count: " + hr_managerCount);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter a non-negative integer");
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen HR manager count: " + hr_managerCount);
-
-            //Logistics Manager Count
-            int logistics_managerCount = 0;
-            success = false;
-            while (!success) {
-                System.out.println("\nHow many logistics managers do you need for this shift?");
-                try {
-                    logistics_managerCount = Integer.parseInt(scanner.nextLine());
-                    if (logistics_managerCount == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else {
-                        System.out.println("Entered count: " + logistics_managerCount);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter a non-negative integer");
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen logistics manager count: " + logistics_managerCount);
-
-            //Shift Manager
-            List<Employee> managers = new ArrayList<>(controller.getAllEmployees()).stream().filter(employee -> employee.certifications.contains(Certifications.ShiftManagement)).collect(Collectors.toList());
-            Set<String> cantWorkEmployees = controller.getConstraint(date, type).employeeIDs;
-            managers = managers.stream().filter(employee -> !cantWorkEmployees.contains(employee.id)).collect(Collectors.toList());
-            if (managers.size() == 0) {
-                System.out.println("No employee who is certified to manage shifts has filled a possibility to work at this shift.");
-                System.out.println("Cannot assign a shift manager. Operation Cancelled");
-                return;
-            }
-            Employee manager = null;
-            success = false;
-            while (!success) {
-                System.out.println("Choose manager for this shift");
-                for (int i = 0; i < managers.size(); i++)
-                    System.out.println((i + 1) + " -- " + managers.get(i).name + ": " + controller.getEmployeeWorkDetailsForCurrentMonth(managers.get(i).id));
-                try {
-                    int ordinal = Integer.parseInt(scanner.nextLine());
-                    if (ordinal == -1) {
-                        System.out.println("Operation Canceled");
-                        return;
-                    } else if (ordinal < 1 || ordinal > managers.size())
-                        System.out.println("Please enter an integer between 1 and " + managers.size());
-                    else {
-                        manager = managers.get(ordinal-1);
-                        System.out.println("Entered manager: " + manager.name);
-                        success = areYouSure();
-                    }
-                } catch (NumberFormatException ex) {
-                    System.out.println("Please enter an integer between 1 and " + managers.size());
-                } catch (Exception ex) {
-                    System.out.println("Unexpected error occurred");
-                    System.out.println("Please try again");
-                }
-            }
-            System.out.println("Chosen shift manager: " + manager.name);
-
-            try {
-                controller.createShift(date, type, manager.id, carrierCount, cashierCount, storekeeperCount, sorterCount, hr_managerCount, logistics_managerCount, );
-                System.out.println("Shift added successfully! Remember to assign employees");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.out.println("Please try again");
-            }
+        PresentationShiftBuilder presentationShiftBuilder = new PresentationShiftBuilder();
+        presentationShiftBuilder.setDate();
+        presentationShiftBuilder.setShiftType();
+        presentationShiftBuilder.setShiftManager();
+        presentationShiftBuilder.setCarrierCount();
+        presentationShiftBuilder.setCashierCount();
+        presentationShiftBuilder.setStorekeeperCount();
+        presentationShiftBuilder.setSorterCount();
+        presentationShiftBuilder.setHr_managerCount();
+        presentationShiftBuilder.setLogistics_managerCount();
+        presentationShiftBuilder.setTransportManagerCount();
+        try {
+            presentationShiftBuilder.buildObject();
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+            addShifts();
         }
     }
 
