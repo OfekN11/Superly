28de42b7d0070f41c23c6378122247a92be4824a
sagiK <kayats@post.bgu.ac.il>
2022-05-19 13:20:53 +0300

    Minor changes to DB

--- dev/SuperLee/src/PresentationLayer/Main.java
+++ dev/SuperLee/src/PresentationLayer/Main.java
@@ -17,13 +17,13 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        BackendController controller = new BackendController();
+        //BackendController controller = new BackendController();
         //ArrayList products = new ArrayList(); products.add(1);
 //        Result addSale = controller.addSale(products, products, 5, LocalDate.of(2022, 05, 15),LocalDate.of(2022, 05, 18));
 //        Result getSale = controller.getSaleHistoryByProduct(1);
         //System.out.println("done");
-//        testSuppliers();
-        new Thread(new MainMenu(controller)).start();
+        testSuppliers();
+        //new Thread(new MainMenu(controller)).start();
     }
 
 
@@ -33,7 +33,7 @@ public class Main {
         SupplierController supplierController = new SupplierController();
 
         try {
-            supplierController.insertFirstDataToDB();
+            //supplierController.insertFirstDataToDB();
             supplierController.loadSuppliersData();
             testOrder(supplierController);
         } catch (Exception exception) {
@@ -44,8 +44,20 @@ public class Main {
     }
 
     private static void testOrder(SupplierController supplierController) {
-        // need to create map and run the createOrders with it();
-        // check if all the data uploads well
+        Map<Integer, Map<Integer, Integer>> shortage = new HashMap<>();
+        Map<Integer, Integer> item1 = new HashMap<>();
+        //Map<Integer, Integer> item2 = new HashMap<>();
+
+        shortage.put(1, item1);
+
+        item1.put(5, 200);
+
+        try{
+            supplierController.createAllOrders(shortage);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
 
