4289cfb9183421b2bf66ac50730cf67c7dc4be5c
HDChaiS <hadch@post.bgu.ac.il>
2022-05-20 14:55:50 +0300

    Add transport display

--- dev/Milestone1/src/Presentation/Objects/Transport/Transport.java
+++ dev/Milestone1/src/Presentation/Objects/Transport/Transport.java
@@ -1,13 +1,83 @@
 package Presentation.Objects.Transport;
 
+import Globals.Enums.ShiftTypes;
+import Globals.Enums.TransportStatus;
+import Globals.Pair;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Transport {
-    public void display()
-    {
-        System.out.println("");
+    private int transportID;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String driverID;
+    private  int truckNumber;
+    private  int truckWeight;
+    private TransportStatus status;
+    private Pair<LocalDate, ShiftTypes> shift;
+
+    public Transport(int transportID, LocalDateTime startTime, LocalDateTime endTime, String driverID, int truckNumber, int truckWeight, Pair<LocalDate, ShiftTypes> shift) {
+        this.transportID = transportID;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.driverID = driverID;
+        this.truckNumber = truckNumber;
+        this.truckWeight = truckWeight;
+        //this.shift = shift;
     }
 
-    public String toString()
+    public void display()
     {
-       return "" ;
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Transport ID: " + transportID);
+        printDates();
+        printCarrier();
+        printTruckDetails();
     }
+    private void printCarrier()
+    {
+        if(driverID.equals(""))
+        {
+            System.out.println("Carrier ID: The carrier has not been assigned yet!");
+        }
+        else
+        {
+            System.out.println("Carrier ID: " + driverID);
+        }
+
+    }
+
+    private void printTruckDetails()
+    {
+        if(truckNumber == -1)
+        {
+            System.out.println("Truck License Number:  The truck has not been installed yet!");
+        }
+        else
+        {
+            System.out.println("Truck License Number: " + truckNumber +
+                    "\nTruck Weight: " + truckWeight);
+        }
+
+    }
+
+    private void printDates()
+    {
+        if(startTime == null)
+        {
+            System.out.println("Start Time: " + startTime);
+        }
+        else {
+        System.out.println("Start Time: The transport has not left yet!");
+    }
+        if(endTime == null)
+        {
+            System.out.println("End Time: " + endTime);
+        }
+        else {
+            System.out.println("End Time: The Transport has not been completed yet!");
+        }
+    }
+
 }
