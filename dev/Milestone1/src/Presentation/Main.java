06991036cac1bdf023004ee4b8610822dc35bc25
Roi Tiefenbrunn <roitief@post.bgu.ac.il>
2022-05-21 21:49:21 +0300

    fix

--- dev/Milestone1/src/Presentation/Main.java
+++ dev/Milestone1/src/Presentation/Main.java
@@ -1,13 +1,9 @@
 package Presentation;
 
 import Presentation.Screens.MainMenu;
-import Presentation.Screens.ShiftsMenu;
 
 public class Main {
     public static void main(String[] args) {
-        //new Thread(new MainMenu(new BackendController())).start();
-
-        ShiftsMenu shiftsMenu = new ShiftsMenu(new BackendController());
-        shiftsMenu.run();
+        new Thread(new MainMenu(new BackendController())).start();
     }
 }
