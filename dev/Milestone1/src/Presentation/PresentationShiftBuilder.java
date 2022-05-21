f3d1578babfac07cfbc75b6fa977719497ddc2c0
Roi Tiefenbrunn <roitief@post.bgu.ac.il>
2022-05-21 14:53:22 +0300

    Known Bugs + shift builder fix

--- dev/Milestone1/src/Presentation/PresentationShiftBuilder.java
+++ dev/Milestone1/src/Presentation/PresentationShiftBuilder.java
@@ -2,7 +2,6 @@ package Presentation;
 
 import Domain.Service.Objects.Employee;
 import Globals.Enums.Certifications;
-import Globals.Enums.JobTitles;
 import Globals.Enums.ShiftTypes;
 
 import java.time.LocalDate;
@@ -16,7 +15,6 @@ import static Globals.util.HumanInteraction.*;
 public class PresentationShiftBuilder {
 
     private static final String NO_LESS = "There can't be less than %o %s in a shift";
-    private static final String EMPLOYEE_LOOP_MSG ="\nHow many %s do you need for this shift?";
     private static final int MIN_CARRIERS= 1;
     private static final int MIN_CASHIERS = 1;
     private static final int MIN_STOREKEEPERS= 1;
@@ -56,6 +54,8 @@ public class PresentationShiftBuilder {
         while (true) {
             System.out.println("\nEnter shift's date");
             date = buildDate();
+            System.out.println("Entered date: " + date.format(dateFormat));
+            System.out.println("Chosen starting date: " + date.format(dateFormat));
             if (areYouSure())
                 return;
         }
@@ -63,55 +63,101 @@ public class PresentationShiftBuilder {
 
     public void setShiftType() throws OperationCancelledException {
         //ShiftType
-        String loopMsg = "\nEnter shift's type";
-        for (int i = 0; i < ShiftTypes.values().length; i++)
-            loopMsg += (i + 1) + " -- " + ShiftTypes.values()[i];
-        loopMsg += "please choose the number of the shift type";
-            type = ShiftTypes.values()[getNumberLoop(loopMsg,1, ShiftTypes.values().length)-1];
+        printExitMassage();
+        while (true) {
+            System.out.println("\nEnter shift's type");
+            for (int i = 0; i < ShiftTypes.values().length; i++)
+                System.out.println((i + 1) + " -- " + ShiftTypes.values()[i]);
+            type = ShiftTypes.values()[(getNumber(1, ShiftTypes.values().length)) - 1];
+            System.out.println("Chosen shift type: " + type);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setCarrierCount() throws OperationCancelledException {
         //Carrier Count
-        String jobName = JobTitles.Carrier+"s";
-        carrierCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
-
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many carriers do you need for this shift?");
+            carrierCount = getNumber(MIN_CARRIERS, String.format(NO_LESS, MIN_CARRIERS, "carriers"));
+            System.out.println("Chosen carrier count: " + carrierCount);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setCashierCount() throws OperationCancelledException {
         //Cashier Count
-        String jobName = JobTitles.Cashier+"s";
-        cashierCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many cashiers do you need for this shift?");
+            cashierCount = getNumber(MIN_CASHIERS, String.format(NO_LESS, MIN_CASHIERS, "cashiers"));
+            System.out.println("Chosen cashier count: " + cashierCount);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setStorekeeperCount() throws OperationCancelledException {
         //Storekeeper Count
-        String jobName = JobTitles.Storekeeper+"s";
-        storekeeperCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many storekeepers do you need for this shift?");
+            storekeeperCount = getNumber(MIN_STOREKEEPERS, String.format(NO_LESS, MIN_STOREKEEPERS, "storekeepers"));
+            System.out.println("Chosen storekeeper count: " + storekeeperCount);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setSorterCount() throws OperationCancelledException {
         //Sorter Count
-        String jobName = JobTitles.Sorter+"s";
-        sorterCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many sorters do you need for this shift?");
+            sorterCount = getNumber(MIN_SORTERS, String.format(NO_LESS, MIN_SORTERS, "sorters"));
+            System.out.println("Chosen sorter count: " + sorterCount);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setHr_managerCount() throws OperationCancelledException {
         //HR Manager Count
-        String jobName = JobTitles.HR_Manager+"s";
-        hr_managerCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many HR managers do you need for this shift?");
+            hr_managerCount = getNumber(MIN_HR_MANAGERS, String.format(NO_LESS, MIN_HR_MANAGERS, "HR managers"));
+            System.out.println("Chosen HR manager count: " + hr_managerCount);
+            if (areYouSure())
+                return;
+        }
     }
 
 
     public void setLogistics_managerCount() throws OperationCancelledException {
         //Logistics Manager Count
-        String jobName = JobTitles.Logistics_Manager+"s";
-        logistics_managerCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many logistics managers do you need for this shift?");
+            logistics_managerCount = getNumber(MIN_LOGISTICS_MANAGERS, String.format(NO_LESS, MIN_LOGISTICS_MANAGERS, "logistics managers"));
+            System.out.println("Chosen logistics manager count: " + logistics_managerCount);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setTransportManagerCount() throws OperationCancelledException {
         //Transport Manager Count
-        String jobName = JobTitles.Transport_Manager+"s";
-        transportManagerCount = getNumberLoop(String.format(EMPLOYEE_LOOP_MSG,jobName),String.format(NO_LESS,MIN_CARRIERS,jobName),MIN_CARRIERS);
+        printExitMassage();
+        while (true) {
+            System.out.println("\nHow many transport managers do you need for this shift?");
+            carrierCount = getNumber(MIN_LOGISTICS_MANAGERS, String.format(NO_LESS, MIN_LOGISTICS_MANAGERS, "storekeepers"));
+            System.out.println("Chosen cashier count: " + carrierCount);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void setShiftManager() throws Exception{
@@ -124,13 +170,16 @@ public class PresentationShiftBuilder {
             System.out.println("Cannot assign a shift manager");
             operationCancelled();
         }
-        String loopString = "Choose manager for this shift";
-        for (int i = 0; i < managers.size(); i++)
-            loopString += (i + 1) + " -- " + managers.get(i).name + ": " + controller.getEmployeeWorkDetailsForCurrentMonth(managers.get(i).id);
-        loopString += "please enter the number of the manager u want";
-        Employee manager = managers.get(getNumberLoop(loopString,1, managers.size()) - 1);
-        managerId = manager.id;
-        System.out.println("Chosen shift manager: " + managerId + " - " + manager.name);
+        while (true) {
+            System.out.println("Choose manager for this shift");
+            for (int i = 0; i < managers.size(); i++)
+                System.out.println((i + 1) + " -- " + managers.get(i).name + ": " + controller.getEmployeeWorkDetailsForCurrentMonth(managers.get(i).id));
+            Employee manager = managers.get(getNumber(1, managers.size()) - 1);
+            managerId = manager.id;
+            System.out.println("Chosen shift manager: " + managerId + " - " + manager.name);
+            if (areYouSure())
+                return;
+        }
     }
 
     public void buildObject(){
@@ -146,22 +195,6 @@ public class PresentationShiftBuilder {
         }
         finally {
             reset();
-        }
-    }
-
-    public int getNumberLoop(String loopMsg, String becauseMsg, int minVal) throws OperationCancelledException {
-        return getNumberLoop(loopMsg,minVal,Integer.MAX_VALUE);
-    }
-
-    public int getNumberLoop(String loopMsg, int minVal,int max) throws OperationCancelledException {
-        printExitMassage();
-        int output;
-        while (true) {
-            System.out.println(loopMsg);
-            output = getNumber(minVal, max);
-            System.out.println("you chose : " +output);
-            if (areYouSure())
-                return output;
         }
     }
 
