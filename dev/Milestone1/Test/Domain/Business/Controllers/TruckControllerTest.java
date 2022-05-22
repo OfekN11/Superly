466e3477058c122919369b4d7e012be44f43cbb3
eldad <vaskere@post.bgu.ac.il>
2022-05-22 14:39:14 +0300

    all function fix

--- dev/Milestone1/Test/Domain/Business/Controllers/TruckControllerTest.java
+++ dev/Milestone1/Test/Domain/Business/Controllers/TruckControllerTest.java
@@ -11,16 +11,15 @@ import static org.junit.Assert.*;
 
 public class TruckControllerTest {
     static TruckController controller = new TruckController();
-    static TruckDAO truckDAO = new TruckDAO();
     private final String NOT_FOUND_MSG = "The truck doesn't exist!";
     @Before
     public void setUp() throws Exception {
-        truckDAO.delete("12345678");
+        controller.removeTruck(12345678);
     }
 
     @After
     public void tearDown() throws Exception {
-        truckDAO.delete("12345678");
+        controller.removeTruck(12345678);
     }
 
     @Test
@@ -28,19 +27,33 @@ public class TruckControllerTest {
         try {
             controller.addTruck(12345678, TruckModel.SemiTrailer, 2, 1);
             controller.removeTruck(12345678);
-            assertNull(truckDAO.get(12345678));
-        }catch(Exception e)
-        {
         }
+        catch (Exception e){
+            assertTrue(false);
+        }
+        try{
+            controller.getTruck(12345678);
+        }
+        catch (Exception e){
+            assertTrue(true);
+        }
+        assertTrue(false);
     }
 
     @Test
     public void addTruck() {
         try {
             controller.addTruck(12345678, TruckModel.SemiTrailer, 2, 1);
-            assertNotNull(truckDAO.get(12345678));
-        }catch(Exception e)
-        {
+        }
+        catch (Exception e){
+            assertTrue(false);
+        }
+        try{
+            controller.getTruck(12345678);
+            assertTrue(true);
+        }
+        catch (Exception e){
+            assertTrue(false);
         }
     }
 
@@ -48,9 +61,14 @@ public class TruckControllerTest {
     public void getTruck() {
         try {
             controller.addTruck(12345678, TruckModel.SemiTrailer, 2, 1);
-            assertEquals(12345670, controller.getTruck(12345670).getLicenseNumber());
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
+            assertTrue(false);
+        }
+        try {
+            controller.getTruck(12345678);
+            assertTrue(true);
+        } catch (Exception e) {
+            assertTrue(false);
         }
     }
 }
