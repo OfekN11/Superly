0d15a4a0703ffc7174518b9590b25ae16bc98836
ChaiShalev18 <71665398+ChaiShalev18@users.noreply.github.com>
2022-06-16 22:30:37 +0300

    Add Base file

--- dev/Milestone3/src/Presentation/WebPresentation/Screens/ViewModels/Transport/Transport/TransportManagementMenu.java
+++ dev/Milestone3/src/Presentation/WebPresentation/Screens/ViewModels/Transport/Transport/TransportManagementMenu.java
@@ -1,11 +1,11 @@
-package Presentation.WebPresentation.Screens.ViewModels.Transport;
+package Presentation.WebPresentation.Screens.ViewModels.Transport.Transport;
 
 import Presentation.WebPresentation.Screens.Screen;
 
 import javax.servlet.Servlet;
 
 public class TransportManagementMenu extends Screen {
-    private static final String greet = "TransportManagement Menu";
+    private static final String greet = "Transport Management Menu";
 
     public TransportManagementMenu() {
         super(greet);
