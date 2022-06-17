2809925ac71716f11a5657e1d78ba77e59013a88
eldad <vaskere@post.bgu.ac.il>
2022-06-17 17:55:03 +0300

    view order update

---
+++ dev/Milestone3/src/Presentation/WebPresentation/Screens/ViewModels/Transport/Transport/Update/AddOrderToTransport.java
@@ -0,0 +1,9 @@
+package Presentation.WebPresentation.Screens.ViewModels.Transport.Transport.Update;
+
+import Presentation.WebPresentation.Screens.Screen;
+
+public class AddOrderToTransport extends Screen {
+    public AddOrderToTransport(String greeting) {
+        super(greeting);
+    }
+}
