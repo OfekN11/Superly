e0dcc86cb301cf49a28f133eb15bd5bf0327a717
maliahamir <amirmal@post.bgu.ac.il>
2022-06-17 14:02:32 +0300

    fix shiftManaegement

--- dev/Milestone3/src/Presentation/WebPresentation/Screens/ViewModels/Transport/Transport/CreateTransport.java
+++ dev/Milestone3/src/Presentation/WebPresentation/Screens/ViewModels/Transport/Transport/CreateTransport.java
@@ -1,6 +1,7 @@
 package Presentation.WebPresentation.Screens.ViewModels.Transport.Transport;
 
 import Presentation.WebPresentation.Screens.Screen;
+import Presentation.WebPresentation.Screens.ViewModels.HR.Login;
 import Presentation.WebPresentation.Screens.ViewModels.Transport.TransportMainMenu;
 
 import javax.servlet.ServletException;
@@ -12,5 +13,15 @@ public class CreateTransport extends Screen {
     private static final String greet = "Transport Management Menu";
     public CreateTransport() {
         super(greet);
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!isAllowed(req, resp)){
+            redirect(resp, Login.class);
+        }
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handleHeader(req, resp);
     }
 }
