c06afcb2b8d2342ae8700f496ecd3deb494fd959
ChaiShalev18 <71665398+ChaiShalev18@users.noreply.github.com>
2022-06-19 04:43:50 +0300

    Update test

--- dev/Milestone3/Test/Domain/Business/Controllers/DocumentControllerTest.java
+++ dev/Milestone3/Test/Domain/Business/Controllers/DocumentControllerTest.java
@@ -1,11 +1,14 @@
 package Domain.Business.Controllers;
 
 import Domain.Business.Controllers.Transport.DocumentController;
+import Domain.Business.Controllers.Transport.TruckController;
 import Domain.Business.Objects.Document.DestinationDocument;
 import Domain.Business.Objects.Document.TransportDocument;
+import Domain.Business.Objects.Truck;
 import Domain.DAL.Abstract.DAO;
 import Domain.DAL.Controllers.TransportMudel.DestinationDocumentDAO;
 import Domain.DAL.Controllers.TransportMudel.TransportDocumentDataMapper;
+import Globals.Enums.TruckModel;
 import InventoryTests.CategoryTests;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.*;
@@ -14,15 +17,15 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DocumentControllerTest {
-    static DocumentController controller;
+public class DocumentControllerTest extends TestCase{
+    private static final InventoryController is =  InventoryController.getInventoryController();
+    static DestinationDocument dDoc  =  new DestinationDocument(303, 1, new ArrayList<>());
+    static TransportDocument tDoc  =  new TransportDocument(202, "18/01/2002", 123, "Chai");
+    private static final DocumentController dc =  new DocumentController();
     static DestinationDocumentDAO destinationDocumentDAO = new DestinationDocumentDAO();
     static TransportDocumentDataMapper transportDocument = new TransportDocumentDataMapper();
-    private DestinationDocument dDoc;
-    private TransportDocument tDoc;
-
     @BeforeAll
-    public static synchronized void setData() {
+    public synchronized static void setData() {
         DAO.setDBForTests(DocumentControllerTest.class);
     }
 
@@ -31,25 +34,31 @@ public class DocumentControllerTest {
         DAO.deleteTestDB(DocumentControllerTest.class);
     }
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        controller = new DocumentController();
-        dDoc  =  new DestinationDocument(303, 1, new ArrayList<>());
-        tDoc  =  new TransportDocument(202, "18/01/2002", 123, "Chai");
-
-    }
-
-    @AfterEach
+    @Override
     public void tearDown() throws Exception {
-        destinationDocumentDAO.remove(303);
-        transportDocument.remove(202);
+        try{
 
+        }catch(Exception e){
+
+        }
     }
+
+    @BeforeEach
+    public void setUp(){
+        try {
+            dc.uploadDestinationDocument(dDoc);
+            dc.uploadTransportDocument(tDoc);
+        } catch (Exception e) {
+
+        }
+    }
+
+
     @Test
     public void testUploadDestinationDocument() {
         try {
-            controller.uploadDestinationDocument(dDoc);
-            dDoc = controller.getDestinationDocument(303);
+            dc.uploadDestinationDocument(dDoc);
+            dDoc = dc.getDestinationDocument(303);
             assertEquals(303, dDoc.getID());
         } catch (Exception e) {
 
@@ -58,7 +67,7 @@ public class DocumentControllerTest {
     @Test
     public void testGetDestinationDocument() {
         try {
-            dDoc = controller.getDestinationDocument(303);
+            dDoc = dc.getDestinationDocument(303);
             assertEquals(303, dDoc.getID());
         } catch (Exception e) {
 
@@ -68,8 +77,8 @@ public class DocumentControllerTest {
     @Test
     public void testUploadTransportDocument() {
         try {
-            controller.uploadTransportDocument(tDoc);
-            tDoc = controller.getTransportDocument(202);
+            dc.uploadTransportDocument(tDoc);
+            tDoc = dc.getTransportDocument(202);
             assertEquals(202, tDoc.getTransportID());
         } catch (Exception e) {
 
@@ -79,7 +88,7 @@ public class DocumentControllerTest {
     @Test
     public void testGetTransportDocument() {
         try {
-            tDoc = controller.getTransportDocument(202);
+            tDoc = dc.getTransportDocument(202);
             assertEquals(202, tDoc.getTransportID());
         } catch (Exception e) {
 
